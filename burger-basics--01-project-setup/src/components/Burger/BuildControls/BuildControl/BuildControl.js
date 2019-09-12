087756dfce46783b9dd45598bc5b2921633f80ee
bhairavee <bhairavichitnis23@gmail.com>
2019-09-12 13:10:22 +0530

    build controols added

---
+++ burger-basics--01-project-setup/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const BuildControl = (props) => (
+    <div>
+        <div>{props.label}</div>
+        <button>Less</button>
+        <button>More</button>
+    </div>
+);
+
+export default BuildControl;
