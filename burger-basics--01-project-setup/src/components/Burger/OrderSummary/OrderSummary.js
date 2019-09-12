4c802bbbcc1647dc33dea0495ea707e33e913202
bhairavee <bhairavichitnis23@gmail.com>
2019-09-12 16:09:36 +0530

    new files

--- burger-basics--01-project-setup/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-basics--01-project-setup/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const OrderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+    .map(igkey => {
+        return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}</li>
+    });
 
+    return (
+        <Aux>
+            <h1>Your Order</h1>
+            <p>
+                A delicious burger with the folowing ingredients:
+            </p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+        </Aux>
+    )
 };
 
 export default OrderSummary;
