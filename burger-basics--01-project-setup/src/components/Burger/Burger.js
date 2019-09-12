bf38a6576940cbd92fe022a8680574caeb171da1
bhairavee <bhairavichitnis23@gmail.com>
2019-09-12 13:04:00 +0530

    few more new files

--- burger-basics--01-project-setup/src/components/Burger/Burger.js
+++ burger-basics--01-project-setup/src/components/Burger/Burger.js
@@ -3,12 +3,25 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) =>  {
+
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => { //Create array based on the no of ingredients selected dynamically (upto col no 52)
+           return <BurgerIngredient key={igKey + i} type={igKey} />
+        }); 
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please Start Adding Ingredients</p>
+    }
+    console.log(transformedIngredients);
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="salad"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="cheese"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
